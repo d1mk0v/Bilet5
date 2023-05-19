4b7baa38eb9afb499d2b8e5e05c9b44a6234c036
d1mk0v <79874763588@ya.ru>
2023-05-19 16:03:06 +0500

    Билет 5

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 //    3. Возвращаем кусок списка с 3-го по 7-й элемент.
 
         int[] arr = {1, 2, 5, 8, 4, 7, 9, 36, 12, 4};
-//        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 arr[i]++;
