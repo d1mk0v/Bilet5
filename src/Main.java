dcbbb68cc67027305e3c190ba2e77bfd2fdfd340
d1mk0v <79874763588@ya.ru>
2023-05-19 16:00:03 +0500

    Билет 5

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         pieceOfList();
     }
 
-
     public static String pieceOfList() {
 
 //    - Задача
@@ -16,17 +15,17 @@ public class Main {
 //    3. Возвращаем кусок списка с 3-го по 7-й элемент.
 
         int[] arr = {1, 2, 5, 8, 4, 7, 9, 36, 12, 4};
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
-                arr[i] ++;
+                arr[i]++;
             }
         }
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
         for (int i = 2; i < 7; i++) {
             String list = new String(String.valueOf(arr[i]));
-            System.out.print(list + ", ");
-//            return pieceOfList();
+//            System.out.print(list + ", ");
+            return pieceOfList();
         }
     }
 }
